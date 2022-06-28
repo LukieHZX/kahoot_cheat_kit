668564ea30edaa79539b77861060864c06220269
LukieHZX <64946972+LukieHZX@users.noreply.github.com>
2022-06-28 11:49:03 +0200

    Update finder.py

--- finder.py
+++ finder.py
@@ -6,13 +6,13 @@ from threading import Thread
 def get(int_range, id):
     for i in int_range:
         if cl.join(i, 'Kahoot-Police') is not False:
-            print(i)
-    print(str(id) + ' is DONE!')
+            print(f'[+] {i}')
+    print('[i] {id} is DONE!')
 
 try:
     ranges = list(range(int(argv[1].split('-')[0]), int(argv[1].split('-')[1])))
 except (IndexError, ValueError):
-    print(f'Type: {sys.executable} {sys.argv[0]} <range>')
+    print(f'[-] Type: {sys.executable} {sys.argv[0]} <range>')
 ranges = [ranges[i:i+250] for i in range(0, len(ranges), 250)]
 
 
@@ -22,4 +22,4 @@ id = 0
 for i in ranges:
     id += 1
     Thread(target=get, args=(i,id)).start()
-print(f'Started {id} Threads')
+print(f'[i] Started {id} Threads')
