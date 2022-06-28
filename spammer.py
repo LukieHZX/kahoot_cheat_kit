27096d10863ddf48a7107b113d9618b97f972431
LukieHZX <64946972+LukieHZX@users.noreply.github.com>
2022-06-28 11:45:25 +0200

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -1,10 +1,10 @@
 from kahoot import client
 
-x = input('NAMES TO SPAM (E.G.: TEST will do: 1. TEST, 2. TESTEST, 3. TESTTESTTEST, etc.\n> )')
-id_game = input('ID OF KAHOOT GAME\n> ')
-for counter, i in enumarate(range(150)):
+x = input('Names to spam (e.g.: TEST will do: 1. TEST, 2. TESTTEST, 3. TESTTESTTEST, ...)\n> ')
+id_game = input('ID of Kahoot-Lobby\n> ')
+c = input('How often do you want to have your bots in the Kahoot-Lobby? (Type a high number for full lobby)\n> ')
+for counter, i in enumarate(range(c)):
     try:
-        client().join(int(id_game), x + x*counter)
-        y += 1
+        client().join(int(id_game), x*counter+1)
     except:
         pass
